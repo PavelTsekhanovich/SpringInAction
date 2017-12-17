437fb9a57eb050227691bee63e7bf5491ffafc4a
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2017-12-17 14:41:30 +0300

    Minor fix.

--- snippets/src/main/java/by/spring/action/snippets/JdbcBoilerplate.java
+++ snippets/src/main/java/by/spring/action/snippets/JdbcBoilerplate.java
@@ -1,100 +1,88 @@
+/*
 package by.spring.action.snippets;
-//<start id="jdbc_JdbcTemplate_long"/>
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class JdbcBpiolerplate {
+
     public Employee getEmployeeById(final long id) {
-        return jdbcTemplate.execute(             //<co id="co_boilerplate_gone"/>
-                "select id, firstname, lastname, salary from " +
-                        "employee where id=?",
-                new PreparedStatementCallback<Employee>() {
-
-                    public Employee doInPreparedStatement( //<co id="co_doInPreparedStatement"/>
-                                                           PreparedStatement stmt)
-                            throws SQLException, DataAccessException {
-
-                        stmt.setLong(1, id);
-                        ResultSet rs = stmt.executeQuery();
-                        if (rs.next()) {
-                            Employee employee = new Employee();
-                            employee.setId(rs.getLong("id"));
-                            employee.setFirstName(rs.getString("firstname"));
-                            employee.setLastName(rs.getString("lastname"));
-                            employee.setSalary(rs.getBigDecimal("salary"));
-                            return employee;
-                        }
-                        return null;
-                    }
-                });
+        return jdbcTemplate.execute("select id, firstname, lastname, salary from employee where id=?", new PreparedStatementCallback<Employee>() {
+            public Employee doInPreparedStatement(PreparedStatement stmt) throws SQLException, DataAccessException {
+                stmt.setLong(1, id);
+                ResultSet rs = stmt.executeQuery();
+                if (rs.next()) {
+                    Employee employee = new Employee();
+                    employee.setId(rs.getLong("id"));
+                    employee.setFirstName(rs.getString("firstname"));
+                    employee.setLastName(rs.getString("lastname"));
+                    employee.setSalary(rs.getBigDecimal("salary"));
+                    return employee;
+                }
+                return null;
+            }
+        });
     }
-//<end id="jdbc_JdbcTemplate_long"/>
 
-    //<start id="jdbc_JdbcTemplate_queryForObject"/>
     public Employee getEmployeeById(long id) {
-        return jdbcTemplate.queryForObject(
-                "select id, firstname, lastname, salary " +   //<co id="co_sql_query"/>
-                        "from employee where id=?",
-                new RowMapper<Employee>() {
-                    public Employee mapRow(ResultSet rs,
-                                           int rowNum) throws SQLException {    //<co id="co_map_the_results"/>
-                        Employee employee = new Employee();
-                        employee.setId(rs.getLong("id"));
-                        employee.setFirstName(rs.getString("firstname"));
-                        employee.setLastName(rs.getString("lastname"));
-                        employee.setSalary(rs.getBigDecimal("salary"));
-                        return employee;
-                    }
-                },
-                id);    // <co id="co_specify_query_params"/>
+        return jdbcTemplate.queryForObject("select id, firstname, lastname, salary from employee where id=?", new RowMapper<Employee>() {
+            public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Employee employee = new Employee();
+                employee.setId(rs.getLong("id"));
+                employee.setFirstName(rs.getString("firstname"));
+                employee.setLastName(rs.getString("lastname"));
+                employee.setSalary(rs.getBigDecimal("salary"));
+                return employee;
+            }
+        }, id);
     }
-//<end id="jdbc_JdbcTemplate_queryForObject"/> 
 
-    //<start id="jdbc_Boilerplate"/>
     public Employee getEmployeeById(long id) {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
         try {
             conn = dataSource.getConnection();
-            stmt = conn.prepareStatement(
-                    "select id, firstname, lastname, salary from " +
-                            "employee where id=?");         //<co id="co_selectEmployee"/>
+            stmt = conn.prepareStatement("select id, firstname, lastname, salary from employee where id=?");
             stmt.setLong(1, id);
             rs = stmt.executeQuery();
             Employee employee = null;
             if (rs.next()) {
-                employee = new Employee();          //<co id="co_createEmployee"/>
+                employee = new Employee();
                 employee.setId(rs.getLong("id"));
                 employee.setFirstName(rs.getString("firstname"));
                 employee.setLastName(rs.getString("lastname"));
                 employee.setSalary(rs.getBigDecimal("salary"));
             }
             return employee;
-        } catch (SQLException e) {  //<co id="co_handleJdbcException"/>
-
+        } catch (SQLException e) {
+            e.printStackTrace();
         } finally {
-            if (rs != null) {                  //<co id="co_cleanUpJdbcMess"/>
+            if (rs != null) {
                 try {
                     rs.close();
                 } catch (SQLException e) {
+                    e.printStackTrace();
                 }
             }
-
             if (stmt != null) {
                 try {
                     stmt.close();
                 } catch (SQLException e) {
+                    e.printStackTrace();
                 }
             }
-
             if (conn != null) {
                 try {
                     conn.close();
                 } catch (SQLException e) {
+                    e.printStackTrace();
                 }
             }
         }
-
         return null;
     }
 }
-//<end id="jdbc_Boilerplate"/> 
+*/
