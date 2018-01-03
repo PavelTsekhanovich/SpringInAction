e61cda15306843cbfbb6832a478b1c2549ae1548
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-01-03 21:55:31 +0300

    Added simple example.

--- spring-aop/src/main/java/by/spring/action/AroundAudience.java
+++ spring-aop/src/main/java/by/spring/action/AroundAudience.java
@@ -1,4 +1,24 @@
 package by.spring.action;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class AroundAudience {
+
+    public void watchPerformance(ProceedingJoinPoint joinPoint) {
+        try {
+            System.out.println("The audience is taking their seats.");
+            System.out.println("The audience is turning off their cellphones");
+
+            long start = System.currentTimeMillis();
+
+            joinPoint.proceed();
+
+            long end = System.currentTimeMillis();
+
+            System.out.println("CLAP CLAP CLAP CLAP CLAP");
+            System.out.println("The performance took " + (end - start) + " milliseconds.");
+        } catch (Throwable throwable) {
+            System.out.println("Boo! We want our money back!");
+        }
+    }
 }
