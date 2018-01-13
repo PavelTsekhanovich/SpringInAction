89cfd66b1406e3a00f9b3df6dd08dcd54008f7fd
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-01-13 21:11:43 +0300

    Added simple example.

--- springidol-aspectj/src/test/java/by/spring/action/MindReaderTest.java
+++ springidol-aspectj/src/test/java/by/spring/action/MindReaderTest.java
@@ -1,4 +1,8 @@
 package by.spring.action;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class MindReaderTest {
 }
