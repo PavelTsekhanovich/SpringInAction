7e7c26b38e4474e875285e1d4dcedb4d6a81bbe9
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-01-17 23:33:56 +0300

    Added simple example.

--- springidol-aspectj/src/test/java/by/spring/action/MindReaderTest.java
+++ springidol-aspectj/src/test/java/by/spring/action/MindReaderTest.java
@@ -1,8 +1,27 @@
 package by.spring.action;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration( { "sneaky-magician.xml" })
 public class MindReaderTest {
+
+    @Autowired
+    MindReader magician;
+
+    @Autowired
+    Thinker volunteer;
+
+    @Test
+    public void magicianShouldReadVolunteersMind() {
+        volunteer.thinkOfSomething("Queen of Hearts");
+
+        assertEquals("Queen of Hearts", magician.getThoughts());
+    }
 }
