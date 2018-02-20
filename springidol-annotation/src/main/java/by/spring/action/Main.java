8c1439b4def46222b00bb6ae2f32a7b48ab82361
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-02-20 16:34:27 +0300

    Minor update

--- springidol-annotation/src/main/java/by/spring/action/Main.java
+++ springidol-annotation/src/main/java/by/spring/action/Main.java
@@ -7,7 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-idol-annotation.xml");
-        Piano piano = (Piano) context.getBean("piano");
-        piano.play();
+        Object obj = context.getBean("piano");
+        if (obj instanceof Piano) {
+            Instrument instrument = (Instrument) obj;
+            instrument.play();
+        }
     }
 }
